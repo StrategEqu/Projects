2b89440493aa85388d4cf9256bbdc858606f5160
Strategy <Dima.kogtev.95@mail.ru>
2023-05-19 14:41:47 +0300

    commit 4

--- Test1/src/Main.java
+++ Test1/src/Main.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
 MyThread thread = new MyThread();
 MyThread thread2 = new MyThread();
 thread.start();
@@ -22,3 +23,16 @@ thread2.start();
         }
     }
 }
+=======
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= 50; i++) {
+            list.add(i);
+        }
+        System.out.println(list);
+        System.out.println(list.get(0));
+        System.out.println(list.get(49));
+        System.out.println(list.size());
+        JFrame frame = new JFrame();
+        }
+    }
+>>>>>>> df6c31a (first commit)
